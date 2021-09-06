f7009cd5c7d349c7dd592689f1f8f6094827d250
Makarov Andrey <winandfx@gmail.com>
2021-09-06 21:32:01 +0300

    RequireJS version

---
+++ typescript/require-config.ts
@@ -0,0 +1,8 @@
+declare var require: any;
+
+require.config({
+    paths: {
+        'knockout': '../node_modules/knockout/build/output/knockout-latest'
+    }
+});
+require(['../typescript/main']); // run main.ts from here.
